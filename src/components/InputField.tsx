c7a7dc6cbc60f36b0449bcabd9ae3669133b1387
Zarko Milanovic <zmilanovic@mscrm-addons.com>
2022-07-11 07:12:09 +0200

    final

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,29 +1,36 @@
-import React,{useRef} from 'react';
-
+import React, { useRef } from "react";
 import "./styles.css";
 
-interface Props{
-    todo:string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>;
-    handleAdd:(e:React.FormEvent)=>void;
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputField : React.FC<Props>=({todo,setTodo, handleAdd}) =>{
 
-  const inputRef= useRef<HTMLInputElement>(null)
-    
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className='input' onSubmit={(e)=>{
-        handleAdd(e)
-        inputRef.current?.blur()
-        }}>
-        <input 
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
         ref={inputRef}
-        value={todo} 
-        onChange={(event)=>setTodo(event.target.value)}
-        type='input' placeholder='Enter a task' className='input__box' />
-        <button className='input_submit' type='submit'>Go</button>
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,36 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
-import { Todo } from '../models/model';
-type Props={
-    todo:Todo
-    todos:Todo[]
-    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
-}
+import { Todo } from "../models/model";
+import { Draggable } from "react-beautiful-dnd";
 
-const SingleTodo:React.FC<Props>=({todo, todos,setTodos})=> {
+const SingleTodo: React.FC<{
+  index: number;
+  todo: Todo;
+  todos: Array<Todo>;
+  setTodos: React.Dispatch<React.SetStateAction<Array<Todo>>>;
+}> = ({ index, todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
-    const handleDelete = (id: number) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-      };
-    
-      const handleDone = (id: number) => {
-        setTodos(
-          todos.map((todo) =>
-            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
-          )
-        )}
-    
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
-    <form className='todos__single' >
-        <span className="todos__single--text">
-            {todo.todo}
-        </span>
-        <div>
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided, snapshot) => (
+        <form
+          onSubmit={(e) => handleEdit(e, todo.id)}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          className={`todos__single ${snapshot.isDragging ? "drag" : ""}`}
+        >
+          {edit ? (
+            <input
+              value={editTodo}
+              onChange={(e) => setEditTodo(e.target.value)}
+              className="todos__single--text"
+              ref={inputRef}
+            />
+          ) : todo.isDone ? (
+            <s className="todos__single--text">{todo.todo}</s>
+          ) : (
+            <span className="todos__single--text">{todo.todo}</span>
+          )}
+          <div>
             <span
               className="icon"
-             
-              
+              onClick={() => {
+                if (!edit && !todo.isDone) {
+                  setEdit(!edit);
+                }
+              }}
             >
               <AiFillEdit />
             </span>
@@ -41,8 +79,10 @@ const SingleTodo:React.FC<Props>=({todo, todos,setTodos})=> {
               <MdDone />
             </span>
           </div>
-    </form>
-  )
-}
+        </form>
+      )}
+    </Draggable>
+  );
+};
 
-export default SingleTodo
+export default SingleTodo;
